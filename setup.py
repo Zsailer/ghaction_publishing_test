cd00e849cac53fcc5b2ae424ee4bfe95f1407128
Zsailer <zachsailer@gmail.com>
2020-04-16 14:59:00 -0700

    bumb version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Zsailer/ghaction_publishing_test'
 EMAIL = 'zachsailer@gmail.com'
 AUTHOR = 'Zach Sailer'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
